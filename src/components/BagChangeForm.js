7629a6468dd8100dd8c93961e8fd794b975fe1b7
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-08 14:38:04 -0800

    Create and style form

---
+++ src/components/BagChangeForm.js
@@ -0,0 +1,66 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function BagChangeForm(props) {
+  const styleCenterText = {
+    textAlign: "center",
+  }
+  const styleForm = {
+    textAlign: "left",
+    borderStyle: "solid",
+    borderRadius: "20px",
+    padding: "20px",
+    marginLeft: "30%",
+    marginRight: "30%"
+  }
+  return (
+    <React.Fragment>
+      <div style={styleCenterText}>
+        <h3>{props.changeMode} Coffee Bag</h3>
+        <form style={styleForm} onSubmit={props.onFormSubmit}>
+        Name: <input
+          type='text'
+          name='name'
+          value={props.bag.name}
+          placeholder='Name/type of beans'
+          required /><br />
+        Origin: <input
+          type='text'
+          name='origin'
+          value={props.bag.origin}
+          placeholder='Origin location of beans'
+          required /><br />
+        Price: $<input
+          type='number'
+          name='pricePerPound'
+          value={props.bag.pricePerPound}
+          placeholder='Price per pound'
+          min='1'
+          required /><br />
+        Roast: <input
+          type='text'
+          name='roast'
+          value={props.bag.roast}
+          placeholder='Light, medium, dark, etc.'
+          required /><br />
+        Current Stock: <input
+          type='number'
+          name='currentPounds'
+          value={props.bag.currentPounds}
+          placeholder='Current pounds of beans in the bag'
+          min='1'
+          required /><br /><br />
+        <div style={styleCenterText}><button type='submit'>{props.changeMode} This Bag</button></div>
+      </form>
+
+
+      </div>
+    </React.Fragment>
+  );
+}
+
+BagChangeForm.propTypes = {
+  changeMode: PropTypes.string,
+  bag: PropTypes.object,
+  onFormSubmit: PropTypes.func
+}
