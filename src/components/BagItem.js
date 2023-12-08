4697a0b082d508235761c376c89bfd64d5de6c94
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-08 12:02:04 -0800

    Make BagItem display function buttons and item data.

---
+++ src/components/BagItem.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+export default function BagItem(props) {
+  const styleText = {
+    textAlign: "left",
+    borderStyle: "solid",
+    borderRadius: "20px",
+    padding: "20px",
+    marginLeft: "30%",
+    marginRight: "30%"
+  }
+  const bagID = props.id;
+  return (
+    <React.Fragment>
+      <br />
+      <div style={styleText}>
+        <h4>Bean: {props.name}</h4>
+        <p>
+          Origin: {props.origin}<br />
+          Price Per Pound: ${props.pricePerPound}<br />
+          Roast: {props.roast}<br />
+          Current # of Pounds: {props.currentPounds}lbs<br />
+        </p>
+        <button onClick={() => props.onSellClick(bagID,1)}>Sell</button>
+        <button onClick={() => props.onEditClick(bagID)}>Edit</button>
+      </div>
+    </React.Fragment>
+  );
+}
+
+BagItem.propTypes = {
+  name: PropTypes.string,
+  origin: PropTypes.string,
+  pricePerPound: PropTypes.number,
+  roast: PropTypes.string,
+  currentPounds: PropTypes.number,
+  id: PropTypes.string,
+  onEditClick: PropTypes.func,
+  onSellClick: PropTypes.func,
+}
