abdda74309819f436f0541e269469e4220874eea
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-08 10:17:29 -0800

    Add branching logic for currentScreen. Add methods to change currentScreen

--- src/components/BeanMachine.js
+++ src/components/BeanMachine.js
@@ -10,23 +10,63 @@ class BeanMachine extends React.Component {
     this.state = {
       displayInventory: true,
       displayBagChange: false,
+      displayBagChangeMode: "Add",
       displayBagDetails: false,
     }
+  };
+
+  handleButtonDisplayInventory = () => {
+    this.setState({
+      displayInventory: true,
+      displayBagChange: false,
+      displayBagDetails: false,
+    })
+  };
+
+  handleButtonDisplayAddBag = () => {
+    this.setState({
+      displayInventory: false,
+      displayBagChange: true,
+      displayBagChangeMode: "Add",
+      displayBagDetails: false,
+    })
   };
 
   render() {
     let currentScreen = null;
 
-    if (this.state.inventoryOpen === true) {
+    if (this.state.displayInventory === true) {
       currentScreen = (
         <React.Fragment>
           <h3>Inventory</h3>
         </React.Fragment>
       )
-
+    } else if (this.state.displayBagDetails === true) {
+      currentScreen = (
+        <React.Fragment>
+          <h3>Coffee Bag Details</h3>
+        </React.Fragment>
+      )
+    } else if (this.state.displayBagChange === true) {
+      let changeMode = "Add";
+      if (this.state.displayBagChangeMode === "Edit") {
+        changeMode = "Edit"
+      }
+      
+      currentScreen = (
+        <React.Fragment>
+          <h3>{changeMode} Coffee Bag</h3>
+        </React.Fragment>
+      )
+    } else {
+      currentScreen = (
+        <React.Fragment>
+          <h3>404 Page Not Found</h3>
+        </React.Fragment>
+      )
     }
 
-    return (
+    return (      
       <React.Fragment>
         {currentScreen}
       </React.Fragment>
