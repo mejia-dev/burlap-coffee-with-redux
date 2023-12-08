6f31494bfac2ae68396e521dd9e970090bb6ed06
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-08 11:41:11 -0800

    Add map to render all bags in inventory as individual BagItem objects

---
+++ src/components/InventoryList.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import BagItem from "./BagItem";
+
+export default function InventoryList(props) {
+  const styleCenterText = {
+    textAlign: "center",
+  }
+  return (
+    <React.Fragment>
+      <h3>Inventory</h3>
+      <div style={styleCenterText}>
+        {props.coffeeBagInventory.map((bag) =>
+          <BagItem
+          name = {bag.name}
+          origin = {bag.origin}
+          pricePerPound = {bag.pricePerPound}
+          roast = {bag.roast}
+          currentPounds = {bag.currentPounds}
+          id = {bag.id}
+          key = {bag.id}
+          onEditClick = {props.buttonActionBagCallEdit}
+          onSellClick = {props.buttonActionBagSellPound}
+          />
+          
+        )};
+      </div>
+    </React.Fragment>
+  );
+}
+
+InventoryList.propTypes = {
+  coffeeBagInventory: PropTypes.array,
+  buttonActionBagCallEdit: PropTypes.func,
+  buttonActionBagSellPound: PropTypes.func
+}
