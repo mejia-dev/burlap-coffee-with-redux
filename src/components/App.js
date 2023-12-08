e3d68c4875a012ecf1fb0e4d6f03b59e657eb404
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-08 09:39:16 -0800

    Render Header and BeanMachine in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React from "react";
+import Header from "./Header";
+import BeanMachine from "./BeanMachine";
+
 
 function App() {
   return (
     <React.Fragment>
-      
+      <Header />
+      <BeanMachine />
     </React.Fragment>
   );
 }
