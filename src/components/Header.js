1867a15477f7668a2cf327331ab3334f5e264fea
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-08 17:11:00 -0800

    Add center text styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 export default function Header() {
+  const styleCenterText = {
+    textAlign: "center",
+  }
   return (
     <React.Fragment>
-      <h1>Burlap Coffee Co. Inventory Tracker</h1>
+      <h1 style={styleCenterText}>Burlap Coffee Co. Inventory Tracker</h1>
     </React.Fragment>
   );
 }
