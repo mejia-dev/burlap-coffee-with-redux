855f332c27b5e814c1496a4622dc1ac676aa3418
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-08 17:13:37 -0800

    Refactor code and clean up unnecessary spaces

--- src/components/BagChangeForm.js
+++ src/components/BagChangeForm.js
@@ -1,8 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function BagChangeForm(props) {
-
+export default function BagChangeForm(props) {
   function createObject(event) {
     event.preventDefault();
     props.onFormSubmit({
@@ -11,9 +10,8 @@ function BagChangeForm(props) {
       pricePerPound: event.target.pricePerPound.value,
       roast: event.target.roast.value,
       currentPounds: event.target.currentPounds.value,
-    },props.currentBag)
+    }, props.currentBag)
   }
-  
   const styleCenterText = {
     textAlign: "center",
   }
@@ -30,42 +28,40 @@ function BagChangeForm(props) {
       <div style={styleCenterText}>
         <h3>{props.changeMode} Coffee Bag</h3>
         <form style={styleForm} onSubmit={createObject}>
-        Name: <input
-          type='text'
-          name='name'
-          defaultValue={props.currentBag.name}
-          placeholder='Name/type of beans'
-          required /><br />
-        Origin: <input
-          type='text'
-          name='origin'
-          defaultValue={props.currentBag.origin}
-          placeholder='Origin location of beans'
-          required /><br />
-        Price Per Pound: $<input
-          type='number'
-          name='pricePerPound'
-          defaultValue={props.currentBag.pricePerPound}
-          placeholder='Price per pound'
-          min='1'
-          required /><br />
-        Roast: <input
-          type='text'
-          name='roast'
-          defaultValue={props.currentBag.roast}
-          placeholder='Light, medium, dark, etc.'
-          required /><br />
-        Current # of Pounds: <input
-          type='number'
-          name='currentPounds'
-          defaultValue={props.currentBag.currentPounds}
-          placeholder='Current pounds of beans in the bag'
-          min='1'
-          required /><br /><br />
-        <div style={styleCenterText}><button type='submit'>{props.changeMode} This Bag</button></div>
-      </form>
-
-
+          Name: <input
+            type='text'
+            name='name'
+            defaultValue={props.currentBag.name}
+            placeholder='Name/type of beans'
+            required /><br />
+          Origin: <input
+            type='text'
+            name='origin'
+            defaultValue={props.currentBag.origin}
+            placeholder='Origin location of beans'
+            required /><br />
+          Price Per Pound: $<input
+            type='number'
+            name='pricePerPound'
+            defaultValue={props.currentBag.pricePerPound}
+            placeholder='Price per pound'
+            min='1'
+            required /><br />
+          Roast: <input
+            type='text'
+            name='roast'
+            defaultValue={props.currentBag.roast}
+            placeholder='Light, medium, dark, etc.'
+            required /><br />
+          Current # of Pounds: <input
+            type='number'
+            name='currentPounds'
+            defaultValue={props.currentBag.currentPounds}
+            placeholder='Current pounds of beans in the bag'
+            min='1'
+            required /><br /><br />
+          <div style={styleCenterText}><button type='submit'>{props.changeMode} This Bag</button></div>
+        </form>
       </div>
     </React.Fragment>
   );
@@ -76,5 +72,3 @@ BagChangeForm.propTypes = {
   currentBag: PropTypes.object,
   onFormSubmit: PropTypes.func
 }
-
-export default BagChangeForm
