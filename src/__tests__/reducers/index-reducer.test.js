ab1278778ccc458b0cc42191b8e3dc13c68bf983
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-15 15:01:24 -0800

    Write and pass second and third tests for rootReducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import inventoryListReducer from "../../reducers/inventory-list-reducer";
+import selectedBagReducer from "../../reducers/selected-bag-reducer";
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
@@ -7,6 +12,14 @@ describe("rootReducer", () => {
       dataBagInventory: {},
       dataBagCurrentlySelected: {}
     });
+  });
+
+  test('Check that initial state of inventoryListReducer matches root reducer', () => {
+    expect(store.getState().dataBagInventory).toEqual(inventoryListReducer(undefined, { type: null }));
+  });
+  
+  test('Check that initial state of selectedBagReducer matches root reducer', () => {
+    expect(store.getState().dataBagCurrentlySelected).toEqual(selectedBagReducer(undefined, { type: null }));
   });
 
 });
