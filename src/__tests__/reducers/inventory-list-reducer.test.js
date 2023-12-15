984f07d6b0b03b0ab5bf747b843cfa6158771212
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-15 10:18:28 -0800

    Write second test for inventory-list-reducer. This test took longer to write due to needing to determine if id is needed or not. It is used in a very specific way in the code and I wanted to make sure passing it to the reducer wouldn't break functionality.

--- src/__tests__/reducers/inventory-list-reducer.test.js
+++ src/__tests__/reducers/inventory-list-reducer.test.js
@@ -1,7 +1,39 @@
 import inventoryListReducer from "../../reducers/inventory-list-reducer";
 
 describe('inventoryListReducer', () => {
+  const sampleBag = {
+    name: "Arabica",
+    origin: "Brazil",
+    pricePerPound: 10,
+    roast: "light",
+    currentPounds: 130,
+    id: 1
+  };
+
   test("Should return default state if no action is specified", () => {
-    expect(inventoryListReducer({}, {type: null})).toEqual({});
+    expect(inventoryListReducer({}, { type: null })).toEqual({});
+  });
+
+  test("Should return default state if no action is specified", () => {
+    const { name, origin, pricePerPound, roast, currentPounds, id } = sampleBag;
+    const action = {
+      type: 'ADD_BAG',
+      name: name,
+      origin: origin,
+      pricePerPound: pricePerPound,
+      roast: roast,
+      currentPounds: currentPounds,
+      id: id
+    }
+    expect(inventoryListReducer({}, { action })).toEqual({
+      [id] : {
+        name: name,
+        origin: origin,
+        pricePerPound: pricePerPound,
+        roast: roast,
+        currentPounds: currentPounds,
+        id: id
+      }
+    });
   });
 });
