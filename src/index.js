ccf351a041430a47be6efc18a4452d8afeb3dee7
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-15 15:11:34 -0800

    Change reducer used from inventoryListReducer to rootReducer

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
-import inventoryListReducer from './reducers/inventory-list-reducer';
+import rootReducer from './reducers/index';
 import { Provider } from 'react-redux';
 
-const store = createStore(inventoryListReducer);
+const store = createStore(rootReducer);
 store.subscribe(() =>
   console.log(store.getState())
 );
