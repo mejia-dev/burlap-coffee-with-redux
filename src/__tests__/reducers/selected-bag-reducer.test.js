30e5d78a48abda06953b304ffb415f79b317da13
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-15 12:27:52 -0800

    Write second test for selected-bag-reducer

--- src/__tests__/reducers/selected-bag-reducer.test.js
+++ src/__tests__/reducers/selected-bag-reducer.test.js
@@ -2,7 +2,36 @@ import selectedBagReducer from "../../reducers/selected-bag-reducer";
 
 describe('selectedBagReducer', () => {
 
+  const sampleBagA = {
+    name: "Arabica",
+    origin: "Brazil",
+    pricePerPound: 10,
+    roast: "light",
+    currentPounds: 130,
+    id: 1
+  };
+  const sampleBagB = {
+    name: "Robusta",
+    origin: "India",
+    pricePerPound: 6,
+    roast: "medium",
+    currentPounds: 130,
+    id: 2
+  };
+
   test("Should return default state if no action is specified", () => {
     expect(selectedBagReducer({}, { type: null })).toEqual({});
+  });
+
+  test("Should set the existing selected bag, replacing any previous value", () => {
+    const action = {
+      type: 'SET_BAG',
+      bag: sampleBagA
+    }
+    expect(selectedBagReducer({ sampleBagB }, action)).toEqual({
+      selectedBag: {
+        sampleBagA
+      }
+    });
   });
 });
