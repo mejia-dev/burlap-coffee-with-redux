7fc4320a31a5897dbe1dd5ee0d68c62b79d25000
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-15 15:58:55 -0800

    Write second test for bag-change-mode-reducer

--- src/__tests__/reducers/bag-change-mode-reducer.test.js
+++ src/__tests__/reducers/bag-change-mode-reducer.test.js
@@ -6,4 +6,13 @@ describe('bagChangeModeReducer', () => {
     expect(bagChangeModeReducer({}, { type: null })).toEqual({});
   });
 
+  test("Should set mode to the defined text, replacing any previous value", () => {
+    const action = {
+      type: 'SET_CHANGE_MODE',
+      mode: "Add"
+    }
+    expect(bagChangeModeReducer({mode: "Edit"}, action)).toEqual({
+      mode: "Add"
+    });
+  });
 });
