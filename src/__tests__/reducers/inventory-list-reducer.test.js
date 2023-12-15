83139c960f2e3ccc0fc606255d03c450518ffadc
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-15 10:36:21 -0800

    Write third test for inventory-list-reducer

--- src/__tests__/reducers/inventory-list-reducer.test.js
+++ src/__tests__/reducers/inventory-list-reducer.test.js
@@ -9,12 +9,22 @@ describe('inventoryListReducer', () => {
     currentPounds: 130,
     id: 1
   };
+  const sampleStateForDeletion = {
+    1: {
+      name: "Bag1",
+      id: 1
+    },
+    2: {
+      name: "Bag2",
+      id: 2
+    }
+  }
 
   test("Should return default state if no action is specified", () => {
     expect(inventoryListReducer({}, { type: null })).toEqual({});
   });
 
-  test("Should return default state if no action is specified", () => {
+  test("Should add a bag to inventory", () => {
     const { name, origin, pricePerPound, roast, currentPounds, id } = sampleBag;
     const action = {
       type: 'ADD_BAG',
@@ -26,13 +36,26 @@ describe('inventoryListReducer', () => {
       id: id
     }
     expect(inventoryListReducer({}, action)).toEqual({
-      [id] : {
+      [id]: {
         name: name,
         origin: origin,
         pricePerPound: pricePerPound,
         roast: roast,
         currentPounds: currentPounds,
         id: id
+      }
+    });
+  });
+
+  test("Should remove a bag from inventory", () => {
+    const action = {
+      type: 'DELETE_BAG',
+      id: 1
+    }
+    expect(inventoryListReducer(sampleStateForDeletion, action)).toEqual({
+      2: {
+        name: "Bag2",
+        id: 2
       }
     });
   });
