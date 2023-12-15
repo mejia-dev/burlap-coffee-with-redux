f97fa7e9ff9a0cab5219eec7f2d10365a176ed0f
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-15 11:06:54 -0800

    Add store.subscribe for logging changes to store

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import inventoryListReducer from './reducers/inventory-list-reducer';
 import { Provider } from 'react-redux';
 
 const store = createStore(inventoryListReducer);
+store.subscribe(() =>
+  console.log(store.getState())
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
