54a4989c2f87fbdda8d6cc1c45bd6e6a4f9662e0
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-15 16:20:18 -0800

    Refactor handleButtonDisplayEditBag to use Redux store

--- src/components/BeanMachine.js
+++ src/components/BeanMachine.js
@@ -60,7 +60,7 @@ class BeanMachine extends React.Component {
   handleButtonDisplayEditBag = (bagID) => {
     const { dispatch } = this.props;
     const selectedBag = Object.values(this.props.dataBagInventory).filter((bag) => bag.id === bagID)[0];
-    const action = {
+    const actionSetSelectedBag = {
       type: 'SET_BAG',
       name: selectedBag.name,
       origin: selectedBag.origin,
@@ -68,12 +68,16 @@ class BeanMachine extends React.Component {
       roast: selectedBag.roast,
       currentPounds: selectedBag.currentPounds,
       id: selectedBag.id
-    }
-    dispatch(action);
+    };
+    dispatch(actionSetSelectedBag);
+    const actionSetChangeMode = {
+      type: 'SET_CHANGE_MODE',
+      mode: "Edit"
+    };
+    dispatch(actionSetChangeMode);
     this.setState({
       displayInventory: false,
       displayBagChange: true,
-      displayBagChangeMode: "Edit",
       displayBagDetails: false,
     })
   };
