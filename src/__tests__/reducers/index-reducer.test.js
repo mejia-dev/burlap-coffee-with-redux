2c1d4781aedad246686b51d7aad2f25588705ca5
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-15 16:06:17 -0800

    Modify first test and write sixth test for rootReducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -2,6 +2,7 @@ import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
 import inventoryListReducer from "../../reducers/inventory-list-reducer";
 import selectedBagReducer from "../../reducers/selected-bag-reducer";
+import bagChangeModeReducer from "../../reducers/bag-change-mode-reducer";
 
 let store = createStore(rootReducer);
 
@@ -10,18 +11,19 @@ describe("rootReducer", () => {
   test('Should return empty state if no known action is inputted', () => {
     expect(rootReducer({}, { type: null })).toEqual({
       dataBagInventory: {},
-      dataBagCurrentlySelected: {}
+      dataBagCurrentlySelected: {},
+      displayBagChangeMode: {}
     });
   });
 
   test('Should confirm that initial state of inventoryListReducer matches rootReducer', () => {
-    expect(store.getState().dataBagInventory).toEqual(inventoryListReducer(undefined, { type: null }));
+    expect(store.getState().dataBagInventory).toEqual(inventoryListReducer({}, { type: null }));
   });
-  
+
   test('Should confirm that initial state of selectedBagReducer matches rootReducer', () => {
-    expect(store.getState().dataBagCurrentlySelected).toEqual(selectedBagReducer(undefined, { type: null }));
+    expect(store.getState().dataBagCurrentlySelected).toEqual(selectedBagReducer({}, { type: null }));
   });
-  
+
   test('Should confirm that ADD_BAG action works the same between inventoryListReducer and rootReducer', () => {
     const action = {
       type: 'ADD_BAG',
@@ -48,6 +50,10 @@ describe("rootReducer", () => {
     }
     store.dispatch(action);
     expect(store.getState().dataBagCurrentlySelected).toEqual(selectedBagReducer({}, action));
+  });
+
+  test('Should confirm that initial state of bagChangeModeReducer matches rootReducer', () => {
+    expect(store.getState().dataBagChangeMode).toEqual(bagChangeModeReducer({}, { type: null }));
   });
 
 });
