e153ef20134ebb57af391578a0bb84b36ff31a33
mejia-dev <aaron.mejia8484@gmail.com>
2023-12-15 16:09:13 -0800

    Pass modified first test for rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import inventoryListReducer from "./inventory-list-reducer";
 import selectedBagReducer from "./selected-bag-reducer";
+import bagChangeModeReducer from "./bag-change-mode-reducer";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   dataBagInventory: inventoryListReducer,
   dataBagCurrentlySelected: selectedBagReducer,
+  displayBagChangeMode: bagChangeModeReducer,
 })
 
 export default rootReducer;
